de24237197f2dcd7e6949997d45f1276a80687d6
AndersKallesoe <anders.kallesoe@boligportal.com>
2021-09-19 21:24:10 +0200

    started implementation of saving system and dynamic number of and size of middle layers. Currently not yet functional

--- weightGen.py
+++ weightGen.py
@@ -4,21 +4,28 @@ import numpy as np
 import json
 
 # np.random.seed(1)
+def generate_weights(layer_dims, save, seed=None):
+    if seed:
+        np.random.seed(seed)
+    weight_dimensions = [(i, o) for i, o in zip(layer_dims[1:], layer_dims[:-1])]
+    weights = [json.dumps((np.random.standard_normal(dim)/dim[1]**.5).tolist()) for dim in weight_dimensions]
+    # weights = {'syn_weights_1': json.dumps((np.random.standard_normal((18, 28 * 28))/(28 * 28)**.5).tolist()),
+    #            'syn_weights_2': json.dumps((np.random.standard_normal((18, 18))/18**.5).tolist()),
+    #            'syn_weights_3': json.dumps((np.random.standard_normal((10, 18))/18**.5).tolist())
+    # }
+    weight_dict = {'syn_weight_' + str(i + 1): w for i, w in zip(list(range(len(weights))), weights)}
 
-weights = {'syn_weights_1': json.dumps((np.random.standard_normal((18, 28 * 28))/(28 * 28)**.5).tolist()),
-           'syn_weights_2': json.dumps((np.random.standard_normal((18, 18))/18**.5).tolist()),
-           'syn_weights_3': json.dumps((np.random.standard_normal((10, 18))/18**.5).tolist())
-}
+    with open(r'saves\%s\weights.json' % save, 'w') as weight_file:
+        json.dump(weight_dict, weight_file)
 
-biases = {
-    'biases_1': json.dumps((np.zeros((18, ))).tolist()),
-    'biases_2': json.dumps((np.zeros((18, ))).tolist()),
-    'biases_3': json.dumps((np.zeros((10, ))).tolist())
-}
+def generate_biases(layer_dims, save):
+    # biases = {
+    #     'biases_1': json.dumps((np.zeros((18, ))).tolist()),
+    #     'biases_2': json.dumps((np.zeros((18, ))).tolist()),
+    #     'biases_3': json.dumps((np.zeros((10, ))).tolist())
+    # }
+    biases_dict = {'syn_weight_' + str(i + 1): json.dumps((np.zeros((b, ))).tolist()) for i, b in zip(list(range(len(layer_dims))), layer_dims[1:])}
 
-with open('weights.json', 'w') as weight_file:
-    json.dump(weights, weight_file)
-
-with open('biases.json', 'w') as biases_file:
-    json.dump(biases, biases_file)
+    with open(r'saves\%s\biases.json' % save, 'w') as biases_file:
+        json.dump(biases_dict, biases_file)
 
